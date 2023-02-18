ae428b61c1e81ce2d97c475c7aa7235eb5cccefe
SouravsGithub <105702695+SouravsGithub@users.noreply.github.com>
2023-02-18 11:30:00 +0530

    the demo seems to be working

--- 2nd-project/app.js
+++ 2nd-project/app.js
@@ -3,6 +3,10 @@ const btn = document.querySelector(".btn");
 const output = document.querySelector(".output");
 const mainWord = document.querySelector(".mainWord");
 const pronounciation = document.querySelector(".pronounciation");
+const li = document.querySelector("li");
+const wordType = document.querySelector(".wordType");
+const definition = document.querySelector(".definition");
+const example = document.querySelector(".example");
 
 btn.addEventListener("click", () => {
     let mainUrl = constructUrl();
@@ -21,7 +25,10 @@ function getData(url) {
     }).then((data) => {
         console.log(data);
         mainWord.innerText = data[0].word;
-        pronounciation.innerText = data[0].phonetic;
+        pronounciation.innerText = data[0].phonetic ? data[0].phonetic : data[0].phonetics[1].text;
+        wordType.innerText = data[0].meanings[0].partOfSpeech;
+        definition.innerText = data[0].meanings[0].definitions[0].definition;
+        example.innerText = data[0].meanings[0].definitions[0].example ? data[0].meanings[0].definitions[0].example : "Don't have any examples of this one";
     }).catch((err) => {
         console.log("error", err);
     });
